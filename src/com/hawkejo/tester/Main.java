2332b6a440ff14cb5b7ba68f263e321f95e5d717
hawkejo <jah7c9@gmail.com>
2017-12-06 17:37:33 -0700

    Removed unused import statements

--- src/com/hawkejo/tester/Main.java
+++ src/com/hawkejo/tester/Main.java
@@ -1,14 +1,5 @@
 package com.hawkejo.tester;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
-import com.hawkejo.complexnumberfft.*;
-import phonecs.WavFile; /* Used for reading in the *.wav files correctly as that
-						   is out of the scope of this demonstration program */
-
 /**
  * The driver program for testing all of the frequency generation classes
  */
